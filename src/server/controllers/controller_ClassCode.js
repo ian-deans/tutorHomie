8a975541265b5e9cbd342b6ee267b752d282ffd1
ideans715@gmail.com <ideans715@gmail.com>
2018-02-28 21:11:09 -0800

    Refactor ClassCode router and controller for mongo

--- src/server/controllers/controller_ClassCode.js
+++ src/server/controllers/controller_ClassCode.js
@@ -1,26 +1,90 @@
 import ClassCode from '../../db/models/ClassCode'
 
+let _currentResponse
+
 export default {
   add: (request, response) => {
+    _currentResponse = response
     const {code, value} = request.body
-    const newClassCode = new ClassCode({
-      code: code,
-      value: value
-    })
 
-    newClassCode.save((error, doc) => {
-      response.json(doc)
-    })
-
-
+    if (code && value) {
+      const newClassCode = new ClassCode({
+        code: code,
+        value: value
+      })
+      newClassCode.save((error, doc) => {
+        if (error) {
+          _dbError(error)
+        }
+        _successfulResponse(doc)
+      })
+    } else {
+      _badRequestError()
+    }
   },
 
   getAll: (request, response) => {
+    _currentResponse = response
     ClassCode.find()
-      .then(results => {
-        response.status(200).json(results)
+      .then(_successfulResponse)
+      .catch(_dbError)
+  },
+
+  getByCode: (request, response) => {
+    _currentResponse = response
+    ClassCode.findOne({code: request.params.code})
+      .then(_successfulResponse)
+      .catch(_dbError)
+  },
+
+  update: (request, response) => {
+    _currentResponse = response
+    ClassCode.findOne({code: request.params.code})
+      .then( classCode => {
+        ClassCode.findByIdAndUpdate(
+          classCode._id,
+          request.body,
+          {new: true}
+        )
+          .then(_successfulResponse)
+          .catch(_dbError)
       })
-      .catch(error => console.log)
+  },
+
+  delete: (request, response) => {
+    _currentResponse = response
+    ClassCode
+      .findOne({
+        code: request.params.code
+      })
+      .remove()
+      .then(
+        _successfulResponse({
+          message: 'Code successfully deleted from database.'
+        })
+      )
+      .catch(_dbError)
+
   }
 
 }
+
+const _successfulResponse = result =>
+  _currentResponse
+    .status(200)
+    .json(result) 
+
+const _dbError = error => 
+  _currentResponse
+    .status(500)
+    .json({
+      message: 'Database error.',
+      error: error,
+    })
+
+const _badRequestError = () =>
+  _currentResponse
+    .status(400)
+    .json({
+      message: 'Invalid request options received from client.'      
+    })
--- src/server/routes/classCodeRouter.js
+++ src/server/routes/classCodeRouter.js
@@ -4,8 +4,8 @@ import controller from '../controllers/controller_ClassCode'
 export default express.Router()
   .post('/', controller.add)
   .get('/', controller.getAll)
-  // .get('/:code', controller.getByCode)
-  // .put('/:code', controller.update)
-  // .delete('/:code', controller.delete)
+  .get('/:code', controller.getByCode)
+  .put('/:code', controller.update)
+  .delete('/:code', controller.delete)
   // .delete('/clean', controller.clearNulls) //TODO: remove this route 
 
