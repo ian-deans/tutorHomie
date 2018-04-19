293a7011b0db2e2808a690b74622921b8dc5575d
Ian Deans <ideans715@gmail.com>
2018-04-18 21:57:40 -0700

    Fix minor errors in SurveyString component

--- nextjs/components/Modal/SurveyString.js
+++ nextjs/components/Modal/SurveyString.js
@@ -2,9 +2,10 @@ import React from 'react'
 import {Alert} from 'react-bootstrap'
 
 export default props => {
+  console.log(props)
   const data = props.data
-  const name = data[1].split(' ')
+  const name = data[2].split(' ')
   return (
-    <Alert>{`${name[1]}, ${name[0]}~${data[3]}~${data[2]}~${data[5]}`}</Alert>
+    <Alert>{`${name[1]}, ${name[0]}~${data[4]}~${data[3]}~${data[0]}`}</Alert>
   )
 }
--- nextjs/components/Table/Row.js
+++ nextjs/components/Table/Row.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default props => {
   return (
-    <tr tabIndex={1} onClick={() => props.popUpFn(props.data[0])}>
+    <tr tabIndex={1} onClick={() => props.popUpFn(props.data[1])}>
       {Object.values(props.data).map((data, i) =>
         <td key={i}>{data}</td>
       )
